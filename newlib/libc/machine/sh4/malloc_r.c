7a1b6035b10476435ff590ee2d359df1dba24c98
diddyholz <sidney.krombholz@gmail.com>
2024-02-25 11:29:15 +0100

    Fix crt0.S and inline malloc

--- newlib/libc/machine/sh4/malloc_r.c
+++ newlib/libc/machine/sh4/malloc_r.c
@@ -4,10 +4,10 @@
 extern void *malloc(size_t size);
 extern void free(void *ptr);
 
-void *_malloc_r(struct _reent *reent, size_t size) {
+inline void *_malloc_r(struct _reent *reent, size_t size) {
     return malloc(size);
 }
 
-void _free_r(struct _reent *reent, void *ptr) {
+inline void _free_r(struct _reent *reent, void *ptr) {
     free(ptr);
 }
